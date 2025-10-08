71d184a5ff9eb996fcbd00c5a5abbe31a3794919
Maciej Bielecki <zyla@prati.pl>
2025-10-08 11:26:14 +0000

    Remove misleading comments

--- plugins/hls-mass-rename-plugin/test/testdata/basic/expected/UseWithoutConstructor.hs
+++ plugins/hls-mass-rename-plugin/test/testdata/basic/expected/UseWithoutConstructor.hs
@@ -6,12 +6,9 @@ module UseWithoutConstructor where
 
 import Data.Text (Text)
 import qualified Data.Text as T
--- Import type WITH constructor - this was automatically added
 import Types1 (Restaurant(..))
 import Types2 (Account(..))
 
--- These functions use OverloadedRecordDot but constructor isn't imported
--- After rename, the import should be updated to Restaurant(..)
 useRestaurantDot :: Restaurant -> Text
 useRestaurantDot r = "R:" <> T.pack (show r.id) <> r.name
 
